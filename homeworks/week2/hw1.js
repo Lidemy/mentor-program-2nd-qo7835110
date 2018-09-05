926030cef0cb7a1cd27c2b7872523ce4a92205e5
qo7835110 <qo7835110@gmail.com>
2018-09-05 21:44:13 +0800

    hw1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,10 @@
 function stars(n) {
-
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        if (n > 30) return;
+        result.push('*'.repeat(i))
+    }
+    return result;
 }
 
 module.exports = stars;
