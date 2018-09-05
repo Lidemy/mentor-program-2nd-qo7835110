a2888ec48548c8227718cd8b24a236d49c7e8722
qo7835110 <qo7835110@gmail.com>
2018-09-05 18:23:49 +0800

    complete hw3~hw5

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,8 @@
 function reverse(str) {
-
+    let str_array = str.split('');
+    let new_array = [];
+    for (let i = str_array.length - 1; i >= 0; i--) {
+        new_array.push(str_array[i])
+    };
+    console.log(new_array.join(''));
 }
