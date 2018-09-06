cb4c5ce999648b9b23a80551f131c1de7f0981d0
qo7835110 <qo7835110@gmail.com>
2018-09-06 15:28:18 +0800

    finish week1

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,8 +1,8 @@
 function printStars(n) {
-    if (n > 30 || n < 1) { return };
+    if (n > 30 || n < 1) return;
     let result = "";
     for (let i = 1; i <= n; i++) {
-        result += '*' + '\n\r';
+        result += '*' + '\n';
     }
     console.log(result);
 }
--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -2,7 +2,7 @@ function printFactor(n) {
     let result = '';
     for (let i = 1; i <= n; i++) {
         if (n % i === 0) {
-            result += i + '\n\r'
+            result += i + '\n'
         }
     }
     console.log(result)
