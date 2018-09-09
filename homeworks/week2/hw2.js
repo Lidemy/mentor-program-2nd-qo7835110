2ebc5d6f91f053922a3362639fd0ffc7078ab4e4
qo7835110 <qo7835110@gmail.com>
2018-09-09 20:35:15 +0800

    finished week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,15 @@
 function alphaSwap(str) {
+    let result = '';
+    let newArray = str.split('');
+    for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i].toUpperCase() === newArray[i]) {
+            newArray[i] = newArray[i].toLowerCase();
+        }
+        else {
+            newArray[i] = newArray[i].toUpperCase()
+        }
+    }
 
+    return (newArray.join(''))
 }
-
 module.exports = alphaSwap
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,5 +1,37 @@
 function add(a, b) {
-  
+    let result = [];
+    let aArray = a.split('').reverse();
+    let bArray = b.split('').reverse();
+    console.log('aArray:' + aArray);
+    console.log('bArray:' + bArray);
+    if (aArray.length > bArray.length) {
+        for (let i = 0; i < aArray.length; i++) {
+            if (bArray[i] === undefined) {
+                bArray[i] = 0;
+            }
+            result.push(parseInt(aArray[i]) + parseInt(bArray[i]))
+        }
+    }
+    else if (bArray.length > aArray.length) {
+        for (let i = 0; i < bArray.length; i++) {
+            if (aArray[i] === undefined) {
+                aArray[i] = 0;
+            }
+            result.push(parseInt(aArray[i]) + parseInt(bArray[i]))
+        }
+    }
+    else if (aArray.length === bArray.length) {
+        for (let i = 0; i < aArray.length; i++) {
+            result.push(parseInt(aArray[i]) + parseInt(bArray[i]))
+        }
+    }
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] >= 10) {
+            result[i] = result[i] - 10;
+            result[i + 1] += 1;
+        }
+    }
+    return (result.reverse().join(''))
 }
-
+console.log(add('12312383813881381381', '129018313819319831'))
 module.exports = add;
