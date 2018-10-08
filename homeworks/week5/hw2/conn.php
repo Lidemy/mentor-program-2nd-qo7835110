1d792d128a30706543a1788d3b01aed72b676550
qo7835110 <qo7835110@gmail.com>
2018-10-09 04:13:45 +0800

    fix conn

--- homeworks/week5/hw2/conn.php
+++ homeworks/week5/hw2/conn.php
@@ -1,8 +1,8 @@
 <?php
 $servername = '166.62.28.131';
-$username = 'student2nd';
-$password = 'mentorstudent123';
-$dbname = 'mentor_program_db';
+$username = '';
+$password = '';
+$dbname = '';
 $conn = new mysqli($servername, $username, $password, $dbname) or die ("連接錯誤<br/>");
 $conn->query("SET NAMES 'UTF8'");
 $conn->query("SET time_zone = '+08:00'");
