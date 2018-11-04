0e92066f1c1dfa65e1741fe0eaf390d4f6f1c434
qo7835110 <qo7835110@gmail.com>
2018-11-05 01:52:48 +0800

    fix logout

--- homeworks/week8/hw2/logout.php
+++ homeworks/week8/hw2/logout.php
@@ -2,7 +2,6 @@
     if (isset($_SESSION['user_id'])){
         unset($_SESSION['user_id']);
         session_destroy();
-        setcookie('PHPSESSID','',time() - 100 );
         header('Location:index.php');
     }
     else{
