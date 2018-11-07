226e722b17c7c42636738566506bc3f4b0b20e41
qo7835110 <qo7835110@gmail.com>
2018-11-08 04:24:10 +0800

    del gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-gulp.task('autoprefixer', function () {
-    var postcss = require('gulp-postcss');
-    var autoprefixer = require('autoprefixer');
-
-    return gulp.src('homeworks\week9\hw1\style.css')
-        .pipe(postcss([autoprefixer()]))
-        .pipe(gulp.dest('./dest'));
-});
