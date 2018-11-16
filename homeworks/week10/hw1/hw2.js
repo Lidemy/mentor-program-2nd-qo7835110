10c220038b1da30a49b7a33f5df3f1ef212b5a3a
qo7835110 <qo7835110@gmail.com>
2018-11-16 22:12:19 +0800

    week10 fix

--- homeworks/week10/hw1/hw2.js
+++ homeworks/week10/hw1/hw2.js
@@ -1,35 +1,39 @@
+'use strict';
+
 function Stack() {
-    let arr = [];
+    var arr = [];
     this.push = function (n) {
         arr[arr.length] = n;
-    }
+    };
     this.pop = function () {
-        let result = arr[arr.length - 1];
+        var result = arr[arr.length - 1];
         arr.splice(arr.length - 1, 1);
         return result;
-    }
+    };
 }
-let stack = new Stack();
+var stack = new Stack();
 stack.push(10);
 stack.push(5);
 console.log('stack:' + stack.pop());
 console.log('stack:' + stack.pop());
 
 function Queue() {
-    let arr = [];
+    var arr = [];
     this.push = function (n) {
         arr[arr.length] = n;
-    }
+    };
     this.pop = function () {
-        let result = arr[0];
+        var result = arr[0];
         arr.splice(0, 1);
         return result;
-    }
+    };
 }
-let queue = new Queue();
+var queue = new Queue();
 queue.push(1);
 queue.push(2);
 console.log('queue:' + queue.pop());
 console.log('queue:' + queue.pop());
-const test = () => console.log(123);
+var test = function test() {
+    return console.log(123);
+};
 test();
